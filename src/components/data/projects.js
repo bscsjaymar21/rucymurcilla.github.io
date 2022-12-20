b71666c075ff185f9a9f78dfe08acb1534c635d6
bscsjaymar21 <119992121+bscsjaymar21@users.noreply.github.com>
2022-12-20 17:32:41 +0800

    Add files via upload

---
+++ src/components/data/projects.js
@@ -0,0 +1,10 @@
+export const ProjectData=[
+    {
+        id:1,
+        title:"My name is Rucy Madelo Muriclla",
+        about: "Im a 4rth year college, BSCS student from North Eastern Mindanao State University Lianga Campus. My hobbies is mentioned in below.",
+        tags:["Dancing", "Singing", "Playing Games"],
+       
+        
+    },
+];
---
+++ src/components/data/work.js
@@ -0,0 +1,11 @@
+export const WorkData = [
+    {
+        company: "NeoDocto" ,
+        designation: "Loan Division",
+        dateJoining: "July 11, 2022",
+        dateEnd: "August 11, 2022",
+        companyLogo:"https://cdn-icons-png.flaticon.com/512/2537/2537338.png",
+        work:"As my experience as intern in our On-Job-Training as a Computer Science and  learned a lot related to computer skills.",
+    },
+   
+]
